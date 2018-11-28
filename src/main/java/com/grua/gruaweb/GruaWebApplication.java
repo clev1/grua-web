d151f7054cc717d5f97ce5f28ebdd360e0c592df
Jamil Pointer <jrpointer@gmail.com>
2018-11-28 16:09:47 -0600

    Initial commit

---
+++ src/main/java/com/grua/gruaweb/GruaWebApplication.java
@@ -0,0 +1,12 @@
+package com.grua.gruaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GruaWebApplication.class, args);
+    }
+}
---
+++ src/test/java/com/grua/gruaweb/GruaWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.grua.gruaweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GruaWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
